2898b6e7e36c9fbaa62f6a3ca8209f72ace931a7
Brett Meyer <bmeyer0127@gmail.com>
2024-03-20 02:02:49 -0400

    Commit before making dev branch

--- Models/Chore.cs
+++ Models/Chore.cs
@@ -1,7 +1,16 @@
-public class Chore 
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Chore
 {
   public int ID { get; set; }
+
+  [Display(Name = "Chore")]
   public string? ChoreName { get; set; }
+
+  [Display(Name = "Person Doing It")]
   public string? ChoreDoer { get; set; }
+
+  [Display(Name = "Completion Status")]
   public string? CompletionStatus { get; set; }
 }
