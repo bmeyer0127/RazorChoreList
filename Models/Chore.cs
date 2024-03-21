a7ba03697a925bd4bcae46af3b57c0e66707a976
Brett Meyer <bmeyer0127@gmail.com>
2024-03-21 00:13:31 -0400

    Added validation for each Chore field. Added radio input for CompletionStatus

--- Models/Chore.cs
+++ Models/Chore.cs
@@ -1,16 +1,22 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc;
 
 public class Chore
 {
   public int ID { get; set; }
 
+  [Required]
   [Display(Name = "Chore")]
   public string? ChoreName { get; set; }
 
+  [Required]
   [Display(Name = "Person Doing It")]
   public string? ChoreDoer { get; set; }
 
+  [Required]
   [Display(Name = "Completion Status")]
+  [BindProperty]
   public string? CompletionStatus { get; set; }
+  public static string[] CompletionStatusOptions = { "Not Completed", "Completed" };
 }
